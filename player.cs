8d41fded78216931d9e3bc01a3f5e1195c2e2465
Bastian <badmaxyt@gmail.com>
2024-02-27 09:15:22 +0100

    feat(player): :sparkles: add movement in c#

--- player.cs
+++ player.cs
@@ -1,12 +1,20 @@
 using Godot;
-using System;
 
 public partial class player : CharacterBody2D
 {
+	[Export]
+	public int Speed { get; set; } = 400;
+
+	public void GetInput()
+	{
+
+		Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		Velocity = inputDirection * Speed;
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
-		var direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-		var velocity = direction * 600;
+		GetInput();
 		MoveAndSlide();
 	}
 }
